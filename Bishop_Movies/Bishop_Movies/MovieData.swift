c9265570428ddbc38489f7d9350f73dae50be1aa
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-11-30 13:18:46 -0600

    Added segue and structs

---
+++ Bishop_Movies/Bishop_Movies/MovieData.swift
@@ -0,0 +1,26 @@
+//
+//  MovieData.swift
+//  Bishop_Movies
+//
+//  Created by Bishop,Mackenzie N on 11/30/22.
+//
+
+import Foundation
+import UIKit
+
+//Movie struct
+struct Movie{
+    let title:String
+    let image:UIImage
+    let releasedYear:String
+    let movieRating:String
+    let boxOffice:String
+    let moviePlot:String
+    let cast:[String]
+}
+
+//Genre struct
+struct Genre{
+    let category:String
+    let movies:[Movie] = []
+}
