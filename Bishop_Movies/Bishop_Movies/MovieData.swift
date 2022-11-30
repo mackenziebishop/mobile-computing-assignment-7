e8752eb377639ab4062f3aecf67f2fcb6cf5319e
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-11-30 15:15:34 -0600

    Added one movie

--- Bishop_Movies/Bishop_Movies/MovieData.swift
+++ Bishop_Movies/Bishop_Movies/MovieData.swift
@@ -22,6 +22,6 @@ struct Movie{
 //Genre struct
 struct Genre{
     let category:String
-    let movies:[Movie] = [
+    let movies:[Movie] = [Movie(title: "After The Thin Man", image: UIImage(named: "after_the_thin_man")!, releasedYear: "1936", movieRating: "7.6", boxOffice: "3.156 M", moviePlot: "Recently returned home from vacation, private detective Nick Charles and his socialite wife, Nora, are back on the case when Nora's cousin, Selma, reports her husband Robert missing. As the duo search for the disreputable Robert, Selma's friend David Graham, who secretly pines for her, aids the investigation. When the three begin to unravel Robert's secret life, the missing persons case turns into one of murder.", cast: ["William Powell", "Myrna Loy", "James Stewart"])
     ]
 }
