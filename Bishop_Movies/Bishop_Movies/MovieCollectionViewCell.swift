927724f870436056bb387843ee787eb779524978
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-11-30 13:05:45 -0600

    Added MovieCollectionViewCell swift file

---
+++ Bishop_Movies/Bishop_Movies/MovieCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieCollectionViewCell.swift
+//  Bishop_Movies
+//
+//  Created by Bishop,Mackenzie N on 11/30/22.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
