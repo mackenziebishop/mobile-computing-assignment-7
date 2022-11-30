d2895e170285e3db12705381fe388019cd9013ce
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-11-30 12:45:51 -0600

    UI Design for Movies App

---
+++ Bishop_Movies/Bishop_Movies/GenreViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Bishop_Movies
+//
+//  Created by Bishop,Mackenzie N on 11/29/22.
+//
+
+import UIKit
+
+class GenreViewController: UIViewController {
+
+    @IBOutlet weak var genreTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
